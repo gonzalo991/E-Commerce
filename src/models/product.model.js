e8720c50ca2a1cb778acaecbfab4620a9d4dcaee
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2022-10-19 20:12:51 -0300

    Creación de modelos

---
+++ src/models/product.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const {Schema, model} = mongoose
+
+const productSchema = new Schema({
+    "id": Number,
+    "modelo": String,
+    "marca": String,
+    "categoria": String,
+    "precio": Number
+})
+
+
+const Product = model("Product", productSchema)
+
+modules.export = Product;
