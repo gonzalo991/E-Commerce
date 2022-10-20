01630026590c4573491e28dc898a53853927c00f
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2022-10-19 21:44:56 -0300

    conexion a base de datos en la nube

--- src/database/db.database.js
+++ src/database/db.database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost:27017/ecommerce';
+const URI = 'mongodb://usqiq9g3lya9aq5levnz:I0tfhegbPVfReGi46Yoo@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bcey26m4851cqal?replicaSet=rs0';
 
 mongoose.connect(URI).
     then(db => console.log('Conexión con la base de datos establecida'))
